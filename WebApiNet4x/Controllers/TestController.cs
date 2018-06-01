83b3dedadd918b768885021aea45110c5f995728
Boris Arturo González Rivera <borisgr04@gmail.com>
2018-06-01 17:28:46 -0500

    Se configura api TestController en .net Framework para autenticar y autorizar api con el permiso Admin.

---
+++ WebApiNet4x/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace WebApiNet4x.Controllers
+{
+    [RoutePrefix("api/test")]
+    public class TestController : ApiController
+    {
+        [Authorize (Roles ="Admin")]
+        public IHttpActionResult Get()
+        {
+            var stringClaimsBuilder = new StringBuilder();
+            foreach (var claim in System.Security.Claims.ClaimsPrincipal.Current.Claims)
+            {
+
+                stringClaimsBuilder.Append($"{claim.Type} {claim.Value}");
+            }
+            stringClaimsBuilder.Append("Änibal el mejor");
+             return Ok(System.Security.Claims.ClaimsPrincipal.Current.Claims ); 
+        }
+    }
+}
---
+++ WebApiNet4x/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.Threading.Tasks;
+using System.Web.Http;
+using IdentityServer3.AccessTokenValidation;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApiNet4x.Startup))]
+
+namespace WebApiNet4x
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            DesactivarLaAsignacionPredeterminadaDeJwt();
+
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = "http://localhost:5000",
+                RequiredScopes = new[] { "Api1" },
+                DelayLoadMetadata = true,
+            });
+
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+            app.UseWebApi(config);
+
+
+            //app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            //app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+            //{
+            //    Authority = "http://localhost:5000",
+            //    ValidationMode = ValidationMode.ValidationEndpoint,
+            //    RequiredScopes = new[] { "Api1" },
+            //    ClientId = "Api1",
+            //    DelayLoadMetadata = true,
+            //    //RequireHttpsMetadata = false,
+            //    ClientSecret = "123654",
+            //    //    options.Authority = "http://localhost:5000";
+            //    //options.RequireHttpsMetadata = false;
+            //    //ApiName = "Api1",
+            //});
+
+            ////configure web api
+            //var config = new HttpConfiguration();
+            //config.MapHttpAttributeRoutes();
+
+            ////require authentication for all controllers
+
+            //config.Filters.Add(new AuthorizeAttribute());
+
+            //app.UseWebApi(config);
+        }
+
+        private static void DesactivarLaAsignacionPredeterminadaDeJwt()
+        {
+            JwtSecurityTokenHandler.InboundClaimTypeMap = new Dictionary<string, string>();
+        }
+    }
+}
